ccd19e7ed9d392821a5f7d87801b73a7cd50de62
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-17 00:34:34 +0530

    welcome screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:gpa_analyzer/screens/welcome_screen.dart';
 
 void main() => runApp(GPAAnalyzer());
 
-class GPAAnalyzer extends StatefulWidget {
-  @override
-  _GPAAnalyzerState createState() => _GPAAnalyzerState();
-}
-
-class _GPAAnalyzerState extends State<GPAAnalyzer> {
+class GPAAnalyzer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        'welcome_screen' : (context) => WelcomeScreen(),
+      },
     );
   }
 }
