d36532227e67019df961d17c349e8c65e2dbb4b6
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-28 20:01:27 +0530

    sharedPreferences works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gpa_analyzer/screen_selector.dart';
+import 'package:gpa_analyzer/screens/home_screen.dart';
 import 'package:gpa_analyzer/welcome_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-  runApp(GPAAnalyzer());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('autologin')?? false;
+  runApp(GPAAnalyzer(flag: isLoggedIn,));
 }
 
 class GPAAnalyzer extends StatelessWidget {
+
+  GPAAnalyzer({@required this.flag});
+  bool flag;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,11 +25,33 @@ class GPAAnalyzer extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id : (context) => WelcomeScreen(),
-        ScreenSelector.id : (context) => ScreenSelector(),
-      },
+      home: flag?ScreenSelector():WelcomeScreen(),
     );
   }
 }
+
+
+// class GPAAnalyzer extends StatefulWidget {
+//
+//   @override
+//   _GPAAnalyzerState createState() => _GPAAnalyzerState();
+// }
+//
+// class _GPAAnalyzerState extends State<GPAAnalyzer> {
+//
+//   bool isLoggedIn = false;
+//   Widget root = WelcomeScreen();
+//
+//   void autoLogIn() async {
+//     final SharedPreferences prefs = await SharedPreferences.getInstance();
+//     isLoggedIn = prefs.getBool('autologin');
+//     if(isLoggedIn) root = ScreenSelector();
+//     print(isLoggedIn);
+//     }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     autoLogIn();
+//     return M
+//   }
+//   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  final _firestore = FirebaseFirestore.instance;
+  String _index;
+  String _batch;
+
+  void getUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    _index = prefs.getString('index');
+    _batch = prefs.getString('batch');
+  }
+
+  void getResults() async {
+    final results = await _firestore.collection('/ucsc/batch16/cs/').doc('1800068').get();
+
+      print(results.data());
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: TextButton(
+        child: Text('get'),
+        onPressed: () {
+          getResults();
+        },
+      ),
+    );
   }
 }
