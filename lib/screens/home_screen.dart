fecd986d7a55aae3026292dbfe7ec1a1dbf0b6fc
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-29 19:54:44 +0530

    gpa calclater added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,31 +10,80 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+  new GlobalKey<RefreshIndicatorState>();
+
   final _firestore = FirebaseFirestore.instance;
   String _index;
   String _batch;
+  String _dep;
+  Map<String, dynamic> results;
+  Map<String, dynamic> gpvs;
+  Map<String, dynamic> credits;
+  double gpa;
+  // void getUserData() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   _index = prefs.getString('index');
+  //   _batch = prefs.getString('batch');
+  //   _dep = prefs.getString('dep');
+  //   print(_index + _dep + _batch);
+  // }
 
-  void getUserData() async {
+  Future<Null> getResults() async {
     final prefs = await SharedPreferences.getInstance();
     _index = prefs.getString('index');
     _batch = prefs.getString('batch');
+    _dep = prefs.getString('dep');
+
+    final resultList = await _firestore.collection('/ucsc/batch$_batch/$_dep/').doc('$_index').get();
+    final gpvList = await _firestore.collection('/ucsc/').doc('gpv').get();
+    final creditList = await _firestore.collection('/ucsc/').doc('${_dep}Credits').get();
+
+    results = resultList.data();
+    gpvs = gpvList.data();
+    credits = creditList.data();
+
+    gpa = getGPA(results, gpvs, credits);
   }
 
-  void getResults() async {
-    final results = await _firestore.collection('/ucsc/batch16/cs/').doc('18000681').get();
+  double getGPA(results, gpvs, credits) {
 
-      print(results.data());
-      
+    var totGPV =0.0 ;
+    var totCREDITS =0 ;
+    var totGPA =0.0 ;
+
+    results.forEach((key, value) {
+      if (gpvs[value]!=null) {
+        var gpv = gpvs[value];
+        var credit = credits[key];
+        totCREDITS += credit;
+        totGPV += gpv*credit;
+      }
+    });
+
+    return totGPA = totGPV/totCREDITS;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // refresh when widget id build
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _refreshIndicatorKey.currentState.show());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(
-        child: Text('get'),
-        onPressed: () {
-          getResults();
-        },
+    return RefreshIndicator(
+      key: _refreshIndicatorKey,
+      onRefresh: getResults,
+      child: ListView(
+        children: [TextButton(
+          child: Text('get'),
+          onPressed: () {
+            getResults();
+          },
+        ),],
       ),
     );
   }
