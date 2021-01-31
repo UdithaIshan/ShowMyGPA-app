948137981cce9680d19b9738eb4135c617f0624e
UdithaIshan <uditha.ishan34@gmail.com>
2021-01-31 23:34:33 +0530

    gpa shows on the screen now

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:circular_custom_loader/circular_custom_loader.dart';
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
-  new GlobalKey<RefreshIndicatorState>();
+      new GlobalKey<RefreshIndicatorState>();
 
   final _firestore = FirebaseFirestore.instance;
   String _index;
@@ -20,14 +19,10 @@ class _HomeState extends State<Home> {
   Map<String, dynamic> results;
   Map<String, dynamic> gpvs;
   Map<String, dynamic> credits;
+  Map<String, dynamic> ranks = {};
   double gpa;
-  // void getUserData() async {
-  //   final prefs = await SharedPreferences.getInstance();
-  //   _index = prefs.getString('index');
-  //   _batch = prefs.getString('batch');
-  //   _dep = prefs.getString('dep');
-  //   print(_index + _dep + _batch);
-  // }
+  double _coveredPercent = 0;
+  String _class = 'N/A';
 
   Future<Null> getResults() async {
     final prefs = await SharedPreferences.getInstance();
@@ -35,33 +30,57 @@ class _HomeState extends State<Home> {
     _batch = prefs.getString('batch');
     _dep = prefs.getString('dep');
 
-    final resultList = await _firestore.collection('/ucsc/batch$_batch/$_dep/').doc('$_index').get();
+    final resultList = await _firestore
+        .collection('/ucsc/batch$_batch/$_dep/')
+        .doc('$_index')
+        .get();
     final gpvList = await _firestore.collection('/ucsc/').doc('gpv').get();
-    final creditList = await _firestore.collection('/ucsc/').doc('${_dep}Credits').get();
+    final creditList =
+        await _firestore.collection('/ucsc/').doc('${_dep}Credits').get();
+    final rankList = await _firestore.collection('/ucsc/batch$_batch/ranks/').doc('${_dep}Rank').get();
 
     results = resultList.data();
     gpvs = gpvList.data();
     credits = creditList.data();
+    ranks = rankList.data();
+
 
     gpa = getGPA(results, gpvs, credits);
+
+    if (!mounted) {
+      return; // Just do nothing if the widget is disposed.
+    }
+
+    setState(() {
+      _coveredPercent = gpa * 25;
+      _class = getClass(gpa);
+    });
   }
 
   double getGPA(results, gpvs, credits) {
-
-    var totGPV =0.0 ;
-    var totCREDITS =0 ;
-    var totGPA =0.0 ;
+    var totGPV = 0.0;
+    var totCREDITS = 0;
+    var totGPA = 0.0;
 
     results.forEach((key, value) {
-      if (gpvs[value]!=null) {
+      if (gpvs[value] != null) {
         var gpv = gpvs[value];
         var credit = credits[key];
         totCREDITS += credit;
-        totGPV += gpv*credit;
+        totGPV += gpv * credit;
       }
     });
 
-    return totGPA = totGPV/totCREDITS;
+    return totGPA = totGPV / totCREDITS;
+  }
+
+  String getClass(gpa) {
+    if (gpa >= 3.50)
+      return 'First Class';
+    else if (gpa < 3.50 && gpa >= 3.25)
+      return 'Second Upper Class';
+    else if (gpa < 3.25 && gpa >= 3.00) return 'Second Lower Class';
+    return 'N/A';
   }
 
   @override
@@ -77,13 +96,51 @@ class _HomeState extends State<Home> {
     return RefreshIndicator(
       key: _refreshIndicatorKey,
       onRefresh: getResults,
-      child: ListView(
-        children: [TextButton(
-          child: Text('get'),
-          onPressed: () {
-            getResults();
-          },
-        ),],
+      child: ListView.builder(
+        padding:
+            EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
+        // children: [
+        //   Align(
+        //     alignment: Alignment.center,
+        //     child: CircularLoader(
+        //       coveredPercent: _coveredPercent,
+        //       width: MediaQuery.of(context).size.width * .6,
+        //       height: MediaQuery.of(context).size.width * .6,
+        //       circleWidth: 12.0,
+        //       circleColor: Colors.grey[300],
+        //       coveredCircleColor: Colors.amber[800],
+        //       circleHeader: 'GPA',
+        //       unit: '/4.00',
+        //       coveredPercentStyle: Theme.of(context)
+        //           .textTheme
+        //           .bodyText1
+        //           .copyWith(
+        //               fontSize: 44.0,
+        //               fontWeight: FontWeight.w900,
+        //               letterSpacing: 1.0,
+        //               color: Colors.black87),
+        //     ),
+        //   ),
+        //   Align(
+        //     alignment: Alignment.center,
+        //     child: Text(_class),
+        //   ),
+        // ],
+        itemCount: ranks.length,
+        itemBuilder: (BuildContext context, int index) {
+          String key = ranks.keys.elementAt(index);
+          return new Column(
+            children: <Widget>[
+              new ListTile(
+                title: new Text("$key"),
+                subtitle: new Text("${ranks[key]}"),
+              ),
+              new Divider(
+                height: 2.0,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
