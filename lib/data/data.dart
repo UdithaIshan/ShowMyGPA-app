cc357b0e8b19c43c7695490649c95fa87c110657
UdithaIshan <uditha.ishan34@gmail.com>
2021-05-03 11:31:28 +0530

    implemented the bloc pattern

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,14 +1,18 @@
 class Data {
-  Data(this.gpa, this.classType, this.rank, this.totalCredits, this.results);
+  Data(this.index, this.username, this.gpa, this.classType, this.rank, this.totalCredits, this.results, this.courses, this.credits);
 
+  String username;
+  String index;
   double gpa;
   String classType;
   String rank;
   int totalCredits;
   Map<String, dynamic> results;
+  Map<String, dynamic> courses;
+  Map<String, dynamic> credits;
 
   static Data fromDataDb(Map<String, dynamic> dbData) {
-    return Data(dbData['gpa'], dbData['classType'], dbData['rank'], dbData['totalCredits'], dbData['results']);
+    return Data(dbData['index'], dbData['username'], dbData['gpa'], dbData['classType'], dbData['rank'], dbData['totalCredits'], dbData['results'], dbData['courses'], dbData['credits']);
   }
 
 }
